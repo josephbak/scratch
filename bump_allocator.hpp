8dc9186a304a764152dd08a55aef73343b08a4da
Joseph Bak <joseph.bak31415@gmail.com>
2026-03-09 21:12:47 +0900

    convert BumpAllocator to template class

--- bump_allocator.hpp
+++ bump_allocator.hpp
@@ -1,35 +1,30 @@
 #include <cstddef>  // std::byte, std::size_t
 
-// // template <typename T>
-// // class BumpAllocator {
-// //     // allocate a block, hand out pointers, free all at once
-
-// };
-
+template <typename T>
 class BumpAllocator {
-    // what data members do you need?
-    std::byte* pool;   // start of the pool
-    std::byte* curr;   // current free position
+    // allocate a block, hand out pointers, free all at once
+    T* pool;   // start of the pool
+    T* curr;   // current free position
     std::size_t cap;   // total capacity
-
 
     public:
 
     BumpAllocator(std::size_t cap)
-    : pool(new std::byte[cap]), curr(pool), cap(cap) {} // member initializer list
+    : pool(new T[cap]), curr(pool), cap(cap) {} // member initializer list
 
     ~BumpAllocator(){
         delete[] pool;
     }
 
-    std::byte* allocate(std::size_t size){
-        if (curr - pool + size <= cap) {
-            curr += size;
-            return curr - size;
+    T* allocate(){
+        if (curr - pool + 1 <= cap) {
+            T* ptr = curr;
+            curr += 1;
+            return ptr;
         }
         return nullptr;
     }
-
+    
     void reset(){
         curr = pool;
     }
--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@ int main(int argc, char* argv[]){
     // auto ball = new BumpAllocator(1024);
     // auto ptr = ball->allocate(sizeof(int));
 
-    BumpAllocator allocator(1024);
+    BumpAllocator<int> allocator(1024);
 
-    for (int i = 0; i < 300; ++i) {
-        allocator.allocate(sizeof(int)); 
-    }
+    // for (int i = 0; i < 300; ++i) {
+    //     allocator.allocate(); 
+    // }
 
-    auto ptr = allocator.allocate(sizeof(int)); // dot, not arrow
-    int* iptr = reinterpret_cast<int*>(ptr);
+    auto iptr = allocator.allocate(); // dot, not arrow
+    // int* iptr = reinterpret_cast<int*>(ptr);
 
     if (iptr) {
     *iptr = 42;
