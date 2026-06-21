a400507a6a96ec226e44d7b4b25fa14278c5b17e
Joseph Bak <joseph.bak31415@gmail.com>
2026-06-21 21:17:22 +0900

    cpp drills: c++ concepts r10

--- cpp-drills/r10.cpp
+++ cpp-drills/r10.cpp
@@ -22,6 +22,85 @@ public:
         }
     }
 
+    // copy constructor
+    SmallVector(const SmallVector& o) : size_(o.size_) {
+        if (o.size_ <= N) {
+            data_ = inline_; capacity_ = N;
+        }else{
+            capacity_ = o.capacity_;
+            data_ = new T[capacity_];
+        }
+        for (std::size_t i = 0; i < size_; ++i) {
+            data_[i] = o.data_[i];   // one loop, both modes
+        }
+    }
+
+    // copy assignment
+    SmallVector& operator=(const SmallVector& o) {
+        if (this == &o) return *this;
+        if (data_ != inline_) {
+            // free if there is existing heap buffer
+            delete[] data_;
+        }
+        size_ = o.size_;
+        if (o.size_ <= N) {
+            data_ = inline_;
+            capacity_ = N;
+        }else {
+            data_ = new T[o.capacity_];
+            capacity_ = o.capacity_;
+        }
+        for (std::size_t i = 0; i < o.size_; ++i) {
+            data_[i] = o.data_[i];
+        }
+        return *this;
+    }
+
+    // move constructor
+    SmallVector(SmallVector&& o) noexcept : size_(o.size_) {
+        if (o.data_ == o.inline_) {
+            // small mode -> can't steal -> must copy
+            // can't transfer ownership of memory that's physically part of another object — it dies when o dies.
+            capacity_ = N;
+            data_ = inline_;
+            // copy
+            for (std::size_t i = 0; i < size_; ++i) {
+                data_[i] = o.data_[i];
+            }
+        } else {
+            capacity_ = o.capacity_;
+            data_ = o.data_; // steal
+            o.data_ = o.inline_;
+            o.size_ = 0;
+            o.capacity_ = N;
+        }
+    }
+
+    // move assignment
+    SmallVector& operator=(SmallVector&& o) noexcept {
+        if (this == &o) return *this;
+        if (data_ != inline_) {
+            // need to free
+            delete[] data_;
+        }
+        if (o.data_ == o.inline_) {
+            capacity_ = N;
+            data_ = inline_;
+            // copy since we can't move
+            for (std::size_t i = 0; i < size_; ++i) {
+                data_[i] = o.data_[i];
+            }
+        } else {
+            capacity_ = o.capacity_;
+            data_ = o.data_; // steal
+            o.data_ = o.inline_;
+            o.size_ = 0;
+            o.capacity_ = N;
+        }
+        return *this;
+    }
+
+
     void push_back(const T& value) {
         if (size_ == capacity_) {
             std::size_t new_cap = capacity_ * 2;
